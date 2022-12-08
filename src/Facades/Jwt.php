07763dfe4102a60d5b53fbf5be888c8fcb74f351
nguyen van hau <nguyenvanhau.soft@gmail.com>
2022-12-08 16:33:13 +0700

    add token jwt

---
+++ src/Facades/Jwt.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GateGem\Core\Facades;
+
+use Illuminate\Support\Facades\Facade;
+/**
+ * 
+ * @method static void setTestTimestamp(int $timestamp = null)
+ * @method static mix registerKeys(array $name)
+ * @method static string encode(array $payload, array $header = [])
+ * @method static array decode(string $token, bool $verify = true)
+ *
+ * @see \GateGem\Core\Facades\Jwt
+ */
+class Jwt extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return \GateGem\Core\Support\JWT\JwtManager::class;
+    }
+}
